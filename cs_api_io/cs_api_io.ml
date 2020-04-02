0398227927da89f55f7aa798b3634e6beaeb556f
Bertrand Bonnefoy-Claudet <bertrand@cryptosense.com>
2020-04-02 12:09:35 +0200

    Add a --no-check-certificate option
    This also fixes the API key header field name.

--- cs_api_io/cs_api_io.ml
+++ cs_api_io/cs_api_io.ml
@@ -3,15 +3,26 @@ let rec convert_to_cohttp_form ?acc:(acc0=[]) form =
   | [] -> acc0
   | (k, v)::t -> convert_to_cohttp_form ~acc:((k, [v])::acc0) t
 
+let get_ctx ~verify =
+  let open Lwt.Infix in
+  Conduit_lwt_unix.init ~verify () >|= fun ctx ->
+  Cohttp_lwt_unix.Client.custom_ctx ~ctx ()
 
-let unsafe_send_request {Api.Request.url; form; method_; header; file} =
-  print_endline ("Requesting " ^ url ^ "...");
+let send_request_exn ~verify {Api.Request.url; form; method_; header; file} =
+  let open Lwt.Infix in
+  let method_str =
+    match method_ with
+    | Get -> "GET"
+    | Post -> "POST"
+  in
+  Printf.printf "Request: %s %s\n%!" method_str url;
   let headers = Cohttp.Header.add_list (Cohttp.Header.init ()) header in
   let url = Uri.of_string url in
+  get_ctx ~verify >>= fun ctx ->
   match method_ with
   | Get
     ->
-    Lwt_result.ok (Cohttp_lwt_unix.Client.get ~headers url)
+    Lwt_result.ok (Cohttp_lwt_unix.Client.get ~ctx ~headers url)
   | Post
     ->
     (
@@ -35,30 +46,35 @@ let unsafe_send_request {Api.Request.url; form; method_; header; file} =
         let co_body =
           Cohttp_lwt.Body.of_stream mp_body
         in
-        Lwt_result.ok (Cohttp_lwt_unix.Client.post ~body:co_body ~headers:co_headers url)
+        Lwt_result.ok (Cohttp_lwt_unix.Client.post ~ctx ~body:co_body ~headers:co_headers url)
       | None
         ->
         let form = convert_to_cohttp_form form in
-        Lwt_result.ok (Cohttp_lwt_unix.Client.post_form ~headers ~params:form url)
+        Lwt_result.ok (Cohttp_lwt_unix.Client.post_form ~ctx ~headers ~params:form url)
     )
 
-let send_request request =
+let send_request ?(verify = true) request =
   Lwt.catch
-    (fun () -> unsafe_send_request request)
-    (function
-      | Failure _
-        ->
-        Lwt_result.fail "URL could not be reached" (* Matching a specific failure results in warnings *)
-      | Tls_lwt.Tls_failure(_)
-        ->
-        Lwt_result.fail "Could not establish HTTPS connection"
-      | Unix.Unix_error(Unix.ECONNREFUSED, "connect", "")
-        ->
-        Lwt_result.fail "Could not establish network connection"
-      | _
-        ->
-        Lwt_result.fail "Unknown error !"
-    )
+    (fun () -> send_request_exn ~verify request)
+    (function error ->
+      let message = match error with
+        | Failure _
+          ->
+          "URL could not be reached" (* Matching a specific failure results in warnings *)
+        | Tls_lwt.Tls_failure _
+          ->
+          "Could not establish HTTPS connection"
+        | Unix.Unix_error (Unix.ECONNREFUSED, "connect", "")
+          ->
+          "Could not establish network connection"
+        | Ssl.Connection_error _
+          ->
+          Printexc.to_string error
+        | _
+          ->
+          Printf.sprintf "Unexpected exception: %s" (Printexc.to_string error)
+      in
+      Lwt_result.fail message)
 
 let get_response (response, body) =
   let open Lwt.Infix in
