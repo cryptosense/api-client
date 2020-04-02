65b0ceebcab4f202ccbfcd8c0c8cceba926230ff
Bertrand Bonnefoy-Claudet <bertrand@cryptosense.com>
2020-04-02 18:54:07 +0200

    Make a release for CentOS 7
    This replaces the release stage with a deploy step for each existing
    job.  As before, the release is only created for tags in the main
    repository.  However, unlike before, two releases are uploaded, one for
    each job ("debian-10" and "centos-7").
    
    For that to work, the release is compiled in Docker instead of directly
    in the Travis VM.

---
+++ .travis-release.sh
@@ -0,0 +1,17 @@
+set -o errexit
+set -o nounset
+set -o pipefail
+set -o xtrace
+
+# Build package
+opam pin add --no-action .
+opam depext cs_api_client
+opam pin add ocaml-conduit
+opam pin add ocaml-cohttp
+opam install cs_api_client
+
+# Create archive
+archive=cs-api-$(git describe --always)-$DISTRO
+mkdir "$archive"
+cp "$(which cs-api)" "$archive/cs-api"
+tar -pcvzf "$archive.tar.gz" "$archive"
