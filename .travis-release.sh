8f2e1df965112933bf8b56bdbaeed647f126738c
Bertrand Bonnefoy-Claudet <bertrand@cryptosense.com>
2020-04-03 12:23:21 +0200

    WIP: bbc2

--- .travis-release.sh
+++ .travis-release.sh
@@ -3,7 +3,14 @@ set -o nounset
 set -o pipefail
 set -o xtrace
 
+# Build package
+opam depext cs_api_client
+opam pin add ocaml-conduit
+opam pin add ocaml-cohttp
+opam pin add .
+
+# Create archive
 archive=cs-api-$(git describe --always)-$DISTRO
 mkdir "$archive"
-cp _build/install/default/bin/cs-api "$archive/cs-api"
+cp "$(which cs-api)" "$archive/cs-api"
 tar -pcvzf "$archive.tar.gz" "$archive"
