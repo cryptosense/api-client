2cd8cd7a05e16f4e90416ff3253ee392b3a051e9
Bertrand Bonnefoy-Claudet <bertrand@cryptosense.com>
2020-04-03 12:23:21 +0200

    WIP: bbc2

--- .travis-release.sh
+++ .travis-release.sh
@@ -3,6 +3,8 @@ set -o nounset
 set -o pipefail
 set -o xtrace
 
+ls -al
+find _build
 archive=cs-api-$(git describe --always)-$DISTRO
 mkdir "$archive"
 cp _build/install/default/bin/cs-api "$archive/cs-api"
